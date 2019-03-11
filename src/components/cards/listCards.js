6744c2bb61364c7472c459c4f90f2cfee60b81ba
Sherin Binu <sherinbinu@hotmail.com>
2019-03-12 01:56:17 +0530

    fixes the issue with resources

--- src/components/cards/listCards.js
+++ src/components/cards/listCards.js
@@ -1,34 +1,41 @@
 import React from 'react';
 
-const ListCards = ({ resource, onClick, upvote, hasVoted }) => {
-
+const ListCards = ({ index, resource, onClick, upvote, hasVoted }) => {
   const trimSnippet = (type, snip) => {
-    if (snip === undefined) return `No ${type}`
-    if (type === "title" && snip.length > 150) {
-      return `${snip.slice(0, 150)}...`
+    if (snip === undefined) return `No ${type}`;
+    if (type === 'title' && snip.length > 150) {
+      return `${snip.slice(0, 150)}...`;
     }
-    if (type === "description" && snip.length > 100) {
-      return `${snip.slice(0, 100)}...`
-    }
-    else return snip
-  }
+    if (type === 'description' && snip.length > 100) {
+      return `${snip.slice(0, 100)}...`;
+    } else return snip;
+  };
 
   return (
     <div className="cardList">
       <div className="row">
         <div className="col-md-1 py-2 text-center">
           <div className="footerButtons voteBtn">
-            <i className={`fas fa-thumbs-up ${(hasVoted === true ? "vb-active" : "")}`} onClick={() => upvote()}></i>{resource.upvotes.length}
+            <i
+              className={`fas fa-thumbs-up ${
+                hasVoted === true ? 'vb-active' : ''
+              }`}
+              onClick={() => upvote()}
+            />
+            {resource.upvotes.length}
           </div>
         </div>
-        <div className="col-md-11 cardList-body" onClick={() => onClick(resource.slug)}>
-          <h2>{trimSnippet("title", resource.meta.title)}</h2>
-          <p> {trimSnippet("description", resource.meta.description)} </p>
+        <div
+          className="col-md-11 cardList-body"
+          onClick={() => onClick(resource.slug, index)}
+        >
+          <h2>{trimSnippet('title', resource.meta.title)}</h2>
+          <p> {trimSnippet('description', resource.meta.description)} </p>
         </div>
         <hr />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ListCards;
--- src/components/cards/masonCards.js
+++ src/components/cards/masonCards.js
@@ -1,38 +1,53 @@
 import React from 'react';
-import './card.css'
+import './card.css';
 
-const MasonCards = ({ resource, onClick, upvote, userId, hasVoted }) => {
+const MasonCards = ({ index, resource, onClick, upvote, userId, hasVoted }) => {
+  const fallbackImage = e => {
+    e.target.src = `https://via.placeholder.com/1500/2e303a/FFFFFF/?text=No%20Image%20Found`;
+  };
 
-    const fallbackImage = (e) => {
-        e.target.src = `https://via.placeholder.com/1500/2e303a/FFFFFF/?text=No%20Image%20Found`
-    }
-
-        return (
-            <div className="p-2" >
-                <div className="card cardRes">
-                    <img className="card-img-top img-respsonsive" src={resource.meta.image} alt={resource.meta.title.slice(0, 15)} onClick={() => onClick(resource.slug)} onError={fallbackImage} />
-                    <div className="card-body ">
-                        <h1 onClick={() => onClick(resource.slug)}>{resource.meta.title}</h1>
-                        <p className="card-text" onClick={() => onClick(resource.slug)}>{resource.meta.description}</p>
-                    </div>
-                    <div className="card-footer text-muted">
-                        <div className="row">
-                            <div className="col-md-6 text-left">
-                                <div className="footerButtons voteBtn">
-                                    <i className={`fas fa-thumbs-up ${hasVoted === true ? "vb-active" : ""}`} onClick={() => upvote()}></i>{resource.upvotes.length}
-                                </div>
-                            </div>
-                            <div className="col-md-6 text-right">
-                                <div className="footerButtons bookmarkBtn">
-                                    <i className={`fas fa-heart`}></i>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <div className="p-2">
+      <div className="card cardRes">
+        <img
+          className="card-img-top img-respsonsive"
+          src={resource.meta.image}
+          alt={resource.meta.title.slice(0, 15)}
+          onClick={() => onClick(resource.slug)}
+          onError={fallbackImage}
+        />
+        <div className="card-body ">
+          <h1 onClick={() => onClick(resource.slug)}>{resource.meta.title}</h1>
+          <p
+            className="card-text"
+            onClick={() => onClick(resource.slug, index)}
+          >
+            {resource.meta.description}
+          </p>
+        </div>
+        <div className="card-footer text-muted">
+          <div className="row">
+            <div className="col-md-6 text-left">
+              <div className="footerButtons voteBtn">
+                <i
+                  className={`fas fa-thumbs-up ${
+                    hasVoted === true ? 'vb-active' : ''
+                  }`}
+                  onClick={() => upvote()}
+                />
+                {resource.upvotes.length}
+              </div>
             </div>
-        )
-    }
+            <div className="col-md-6 text-right">
+              <div className="footerButtons bookmarkBtn">
+                <i className={`fas fa-heart`} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MasonCards;
